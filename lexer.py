c12472bd42fec34e81d1200e3e31fc31f5d6b9c6
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-11-13 23:15:18 +0000

    Palabras reservadas & exp. regular para declarar variables

--- lexer.py
+++ lexer.py
@@ -2,33 +2,86 @@ import ply.lex as lex
 
 #LP asignado : PHP
 
+#palabras reservadas
+
 reserved = {
-    'if': 'IF',
-    'break' : 'BREAK',
+    'abstract': 'ABSTRACT',
+    'and': 'AND',
+    'as': 'AS',
+    'array': 'ARRAY',
+    'break': 'BREAK',
+    'case': 'CASE',
+    'catch': 'CATCH',
+    'class': 'CLASS',
+    'clone': 'CLONE',
+    'callable': 'CALLABLE',
+    'const': 'CONST',
+    'continue': 'CONTINUE',
+    'declare': 'DECLARE',
+    'default': 'DEFAULT',
+    'die': 'DIE',
+    'do': 'DO',
+    'echo': 'ECHO',
+    'else': 'ELSE',
+    'elseif': 'ELSEIF',
+    'empty': 'EMPTY',
+    'endswitch': 'ENDSWITCH',
+    'endif': 'ENDIF',
+    'endforeach': 'ENDFOREACH',
+    'endfor': 'ENDFOR',
+    'enddeclare': 'ENDDECLARE',
+    'eval': 'EVAL',
+    'extends': 'ENTENDS',
     'final': 'FINAL',
+    'finally': 'FINALLY',
+    'for': 'FOR',
+    'foreach': 'FOREACH',
+    'function': 'FUNCTION',
+    'global': 'GLOBAL',
+    'goto': 'GOTO',
+    'if': 'IF',
+    'implements': 'IMPLEMENTS',
+    'include': 'INCLUDE',
+    'include_once': 'INCLUDE_ONCE',
+    'instanceof': 'INSTANCEOF',
+    'insteadof': 'INSTEADOF',
+    'interface': 'INTERFACE',
+    'isset': 'ISSET',
+    'list': 'LIST',
+    'match': 'MATCH',
+    'namespace': 'NAMESPACE',
+    'new': 'NEW',
+    'or': 'OR',
     'print': 'PRINT',
+    'private': 'PRIVATE',
+    'protected': 'PROTECTED',
+    'require_once': 'REQUIRE_ONCE',
     'return': 'RETURN',
-    'for': 'FOR'
+    'static': 'STATIC',
+    'require': 'REQUIRE',
+    'public': 'PUBLIC',
+    'switch': 'SWITCH',
+    'throw': 'THROW',
+    'trait': 'TRAIT',
+    'try': 'TRY',
+    'unset': 'UNSET',
+    'var': 'VAR',
+    'while': 'WHILE',
+    'yield': 'YIELD',
+    'use': 'USE',
+    'yieldfrom': 'YIELDFROM',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'null': 'NULL',
+    'void': 'VOID',
+    'iterable': 'ITERABLE',
+    'object': 'OBJECT'
 }
 
 # List of token names.   This is always required
-tokens = (
-             'NUMBER',
-             'MAS',
-             'MINUS',
-             'TIMES',
-             'DIVIDE',
-             'LPAREN',
-             'RPAREN',
-             'FLOTANTE',
-             'VARIABLE',
-             'STRING',
-             'EQUALS',
-             'SAME',
-             'MENORQUE',
-             'MAYORQUE',
-             'DOUBLE'
-         ) + tuple(reserved.values())
+tokens = ('NUMBER', 'MAS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
+          'FLOTANTE', 'VARIABLE', 'STRING', 'EQUALS', 'SAME', 'MENORQUE',
+          'MAYORQUE', 'DOUBLE') + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
 t_MAS = r'\+'
@@ -39,40 +92,48 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_NUMBER = r'\d+'
 t_FLOTANTE = r'\d+\.\d+'
-t_EQUALS = r'==' #son iguales
-t_SAME = '===' #son iguales y del mismo tipo (el mismo objeto)
+t_EQUALS = r'=='  #son iguales
+t_SAME = '==='  #son iguales y del mismo tipo (el mismo objeto)
 t_MAYORQUE = r'>'
 t_MENORQUE = r'<'
 
 # t_VARIABLE = r'[a-z]+'
+
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
+#Define una variable
 def t_VARIABLE(t):
-    r'[a-zA-Z]+'
+    r'\$([a-zA-Z]|_)([a-zA-Z]|\d|_)*'
     t.type = reserved.get(t.value, 'VARIABLE')
     return t
+
 
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
 
+
 def t_STRING(t):
-  r'\'.+\''
-  t.type = reserved.get(t.value, 'STRING')
-  return t
+    r'\'.+\''
+    t.type = reserved.get(t.value, 'STRING')
+    return t
+
 
 def t_DOUBLE(t):
-  r'(\d+,\d+)+'
-  t.type = reserved.get(t.value, 'DOUBLE')
-  return t
+    r'(\d+,\d+)+'
+    t.type = reserved.get(t.value, 'DOUBLE')
+    return t
+
 
 # Error handling rule
 def t_error(t):
     print("Componente léxico no reconocido '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 # Build the lexer
 lexer = lex.lex()
