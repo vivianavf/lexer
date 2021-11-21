859ba55a8d5c36b5ae7397e3f0eb46498dd0698d
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-21 15:13:44 -0500

    fix: merge

--- lexer.py
+++ lexer.py
@@ -5,157 +5,139 @@ import ply.lex as lex
 #palabras reservadas
 
 reserved = {
-    'abstract': 'ABSTRACT',
     'and': 'AND',
-    'as': 'AS',
     'array': 'ARRAY',
     'break': 'BREAK',
-    'case': 'CASE',
-    'catch': 'CATCH',
     'class': 'CLASS',
-    'clone': 'CLONE',
-    'callable': 'CALLABLE',
-    'const': 'CONST',
     'continue': 'CONTINUE',
-    'declare': 'DECLARE',
-    'default': 'DEFAULT',
-    'die': 'DIE',
-    'do': 'DO',
     'echo': 'ECHO',
     'else': 'ELSE',
     'elseif': 'ELSEIF',
-    'empty': 'EMPTY',
-    'endswitch': 'ENDSWITCH',
-    'endif': 'ENDIF',
-    'endforeach': 'ENDFOREACH',
-    'endfor': 'ENDFOR',
-    'enddeclare': 'ENDDECLARE',
-    'eval': 'EVAL',
-    'extends': 'ENTENDS',
-    'final': 'FINAL',
-    'finally': 'FINALLY',
     'for': 'FOR',
-    'foreach': 'FOREACH',
     'function': 'FUNCTION',
-    'global': 'GLOBAL',
-    'goto': 'GOTO',
     'if': 'IF',
-    'implements': 'IMPLEMENTS',
-    'include': 'INCLUDE',
-    'include_once': 'INCLUDE_ONCE',
-    'instanceof': 'INSTANCEOF',
-    'insteadof': 'INSTEADOF',
-    'interface': 'INTERFACE',
-    'isset': 'ISSET',
-    'list': 'LIST',
-    'match': 'MATCH',
-    'namespace': 'NAMESPACE',
     'new': 'NEW',
     'or': 'OR',
-    'print': 'PRINT',
     'private': 'PRIVATE',
     'protected': 'PROTECTED',
-    'require_once': 'REQUIRE_ONCE',
     'return': 'RETURN',
-    'static': 'STATIC',
-    'require': 'REQUIRE',
     'public': 'PUBLIC',
-    'switch': 'SWITCH',
-    'throw': 'THROW',
-    'trait': 'TRAIT',
-    'try': 'TRY',
-    'unset': 'UNSET',
     'var': 'VAR',
     'while': 'WHILE',
-    'yield': 'YIELD',
-    'use': 'USE',
-    'yieldfrom': 'YIELDFROM',
     'true': 'TRUE',
     'false': 'FALSE',
     'null': 'NULL',
-    'void': 'VOID',
-    'iterable': 'ITERABLE',
-    'object': 'OBJECT'
+    'object': 'OBJECT',
+    'php': "PHP",
+    'this': "THIS"
+    
 }
 
 # List of token names.   This is always required
-tokens = ('ENTERO', 'MAS', 'MENOS', 'MULTIPLICACION', 'DIVISION', 'LPAREN', 'RPAREN',
-          'FLOTANTE', 'VARIABLE', 'ASIGNACION', 'STRING', 'EQUALS', 'SAME', 'MENORQUE', 'FINAL_DE_LINEA',
-          'MAYORQUE', 'DOUBLE','LLLAVE','RLLAVE') + tuple(reserved.values())
+tokens = ('ENTERO', 'MAS', 'MENOS', 'MULTIPLICACION', 'DIVISION', 'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET','NEGACION', "COMA","ASIGNACION2","PREGUNTA","DOLLAR","NUMERO","DOS_PUNTOS","AUTOINCREMENTO","AUTODECREMENTO","IDENTIFIER","CONCAT_ASIG",
+    'FLOTANTE', 'VARIABLE', 'ASIGNACION', 'STRING1','STRING2', 'EQUALS', 'SAME', 'MENORQUE', 'FINAL_DE_LINEA', 'ID',"AMPERSANT","HASHTAG","DOT", "COMILLASIMPLE", "COMILLASDOBLES","ARROW",
+          'MAYORQUE', 'DOUBLE','LLLAVE','RLLAVE', 'ASIGNACION_AUMENTADA', 'ASIGNACION_DISMINUIDA', 'DESIGUALDAD', 'MAYORIGUAL', 'MENORIGUAL', 'COMENTARIO') + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
+#simbolor matematicos
+t_AUTOINCREMENTO = r'\+\+'
+t_AUTODECREMENTO = r'--'
 t_MAS = r'\+'
 t_MENOS = r'-'
 t_MULTIPLICACION = r'\*'
 t_DIVISION = r'/'
+t_ASIGNACION_AUMENTADA = r'\+='
+t_ASIGNACION_DISMINUIDA = r'-='
+
+# simpbolos de agrupacion
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_ENTERO = r'\d+'
-t_FLOTANTE = r'\d+\.\d+'
-t_FINAL_DE_LINEA = r';'
-t_ASIGNACION = r'='
-t_EQUALS = r'=='  #son iguales
-t_SAME = '==='  #son iguales y del mismo tipo (el mismo objeto)
-t_MAYORQUE = r'>'
-t_MENORQUE = r'<'
 t_LLLAVE = r'\{'
 t_RLLAVE = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+
+#primitivos
+#  t_ENTERO = r'\d+'
+#  t_FLOTANTE = r'\d+\.\d+'
+
+
+# ASIGNACION
+t_ASIGNACION = r'='
+t_ASIGNACION2 = r'=>'
+t_CONCAT_ASIG = r'\.='
+
+# comparaciones
+t_SAME = '==='  #son iguales y del mismo tipo (el mismo objeto)
+t_EQUALS = r'=='  #son iguales
+t_DESIGUALDAD = r'!='
+t_MAYORIGUAL = r'>='
+t_MENORIGUAL = r'<='
+t_MAYORQUE = r'>'
+t_MENORQUE = r'<'
+t_NEGACION = r'!'
+
+# carateres 
+t_DOLLAR = r'\$'
+t_PREGUNTA = r'\?'
+t_AMPERSANT = r'\&'
+t_HASHTAG = r'\#'
+t_DOT = r'\.'
+t_COMA = r','
+# t_COMILLASIMPLE = r'\''
+# t_COMILLASDOBLES = r'\"'
+t_FINAL_DE_LINEA = r';'
+t_DOS_PUNTOS = r':'
+t_ARROW =r'->'
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-
-
+#NUMERO, flotante o entero
+def t_NUMERO(t):
+    r'\d+(\.\d+)?'
+    t.value = float(t.value)
+    return t
 #Define una variable
 def t_VARIABLE(t):
     r'\$([a-zA-Z]|_)([a-zA-Z]|\d|_)*'
     t.type = reserved.get(t.value, 'VARIABLE')
+    return t
+#Define una variable
+def t_IDENTIFIER(t):
+    r'([a-zA-Z]|_)([a-zA-Z]|\d|_)*'
+    t.type = reserved.get(t.value, 'IDENTIFIER')
+    return t
+#Define ID
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
 
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
 
+# cadena que usa comillas dobles
+def t_STRING1(t):
+    r'\"([^\\\n]|(\\.))*?\"'
+    return t
 
-def t_STRING(t):
-    r'("|\').*("|\')'
-    t.type = reserved.get(t.value, 'STRING')
+# cadena que usa comillas simples
+def t_STRING2(t):
+    r'\'([^\\\n]|(\\.))*?\''
+    # r'\'([^\'].)*\''
     return t
 
 
-def t_DOUBLE(t):
-    r'(\d+,\d+)+'
-    t.type = reserved.get(t.value, 'DOUBLE')
-    return t
 
-def t_IF(t):
-  r'if\(.+\)'
-  t.type = reserved.get(t.value, 'IF')
+
+
+def t_COMENTARIO(t):
+  r'(//.*)|(/\*.*\*/)|(\#.*)'
+  t.type = reserved.get(t.value, 'COMENTARIO')
   return t
-
-def t_ECHO(t):
-    r'echo .+'
-    t.type = reserved.get(t.value, 'ECHO')
-    return t
-
-def t_ELSE(t):
-  r'else\{.+\}'
-  t.type = reserved.get(t.value, 'ELSE')
-  return t
-
-def t_RETURN(t):
-    r'return.*'
-    t.type = reserved.get(t.value, 'RETURN')
-
-def t_USE(t):
-    r'use .+'
-    t.type = reserved.get(t.value, 'USE')
-
-def t_LIST(t):
-    r'list\($([a-zA-Z]|_)([a-zA-Z]|\d|_)*,+\)'
-    t.type = reserved.get(t.value, 'LIST')
-
 
 # Error handling rule
 def t_error(t):
@@ -167,25 +149,16 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test it out
-data = '''
-$a = 3;
-$b = 2;
-$c = $a+$b;
-$d = $a-$b;
+f = open("index.php")
+data = ''.join(f.readlines())
 
-if($c>$d){
-  echo "la variable a es mayor a b";
-}else{
-  echo "la variable a es menor a b";
-}
-'''
-
+if __name__ =="__main__":
 # Give the lexer some input
-lexer.input(data)
+    lexer.input(data)
 
 # Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
